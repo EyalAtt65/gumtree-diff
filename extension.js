520f1804c5bc87b9b1e84b6a1dfc930e39bfa295
EyalAtt65 <eyaleyal65@gmail.com>
2023-02-27 12:38:36 +0200

    add java wrapper

--- extension.js
+++ extension.js
@@ -5,6 +5,19 @@ const vscode = require('vscode');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
+function run_java(src_filepath, dst_filepath) {
+	let java = require('java');
+
+	// TODO: replace values here with actual names once available
+	java.classpath.push("build_dir")
+
+	let result = java.import("ClassName")
+
+	let instance = java.newInstanceSync("ClassName")
+
+	result = java.callStaticMethodSync("className","methodName",[src_filepath,dst_filepath])
+}
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -12,27 +25,27 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gumtree-diff" is now active!');
+	console.log('Congratulations, your extension "gumtree-diff" is now active!')
 
-	let source_uri = null;
+	let source_uri = null
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('gumtree-diff.source_select', function (uri) {
-		vscode.window.showInformationMessage(`source file selected: ${uri.fsPath}`);
+		vscode.window.showInformationMessage(`source file selected: ${uri.fsPath}`)
 		source_uri = uri
 	});
 
 	disposable = vscode.commands.registerCommand('gumtree-diff.dest_select', function (dest_uri) {
-		vscode.window.showInformationMessage(`destination file selected: ${dest_uri.fsPath}`);
+		vscode.window.showInformationMessage(`destination file selected: ${dest_uri.fsPath}`)
 		if (source_uri == null) {
 			vscode.window.showInformationMessage("[ERROR] no source file selected")
 		}
-		vscode.window.showInformationMessage(`comparing files: source-${source_uri.fsPath}, dest-${dest_uri.fsPath}`);
+		vscode.window.showInformationMessage(`comparing files: source-${source_uri.fsPath}, dest-${dest_uri.fsPath}`)
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
 
 // This method is called when your extension is deactivated
