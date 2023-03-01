20f9b5907baa20922b9d946f2075430707c122e4
Ben Shani <benshani04@gmail.com>
2023-03-01 11:38:14 +0200

    Added 2 tab split and basic colors

--- extension.js
+++ extension.js
@@ -43,11 +43,66 @@ function activate(context) {
 			vscode.window.showInformationMessage("[ERROR] no source file selected")
 		}
 		// vscode.window.showInformationMessage(`comparing files: source-${source_uri.fsPath}, dest-${dest_uri.fsPath}`)
-		let newtab = new vscode.TabInputTextDiff(source_uri,dest_uri)
+		// let newtab = new vscode.TabInputTextDiff(source_uri,dest_uri)		
 		// vscode.workspace.openTextDocument(newtab)
-		vscode.window.showTextDocument(newtab.modified)
+		// vscode.window.showTextDocument(newtab.modified)
+		// let newtab = new vscode.TabInputTextDiff(source_uri, source_uri)
+		// let newtab2 = new vscode.TabInputTextDiff(dest_uri, dest_uri)
+		// vscode.window.showTextDocument(newtab.modified)
+		// vscode.window.showTextDocument(newtab2.modified, {viewColumn: vscode.ViewColumn.Beside})
+
+		vscode.workspace.openTextDocument(source_uri).then(src_doc => {
+			vscode.window.showTextDocument(src_doc).then(src_editor => {
+				vscode.workspace.openTextDocument(dest_uri).then(dest_doc => {
+					vscode.window.showTextDocument(dest_doc, {viewColumn: vscode.ViewColumn.Beside}).then(dest_editor => {
+							decorate(src_editor, dest_editor)
+					})
+				})
+			})
+		})
 	});
 }
+
+function decorate(editor0, editor1) {
+	let sourceCode = editor0.document.getText()
+
+	let greenArray = []
+	let range1 = new vscode.Range(new vscode.Position(0, 3),
+								 new vscode.Position(3, 10))
+	let greenDecoration = {range: range1}
+	greenArray.push(greenDecoration)
+	editor0.setDecorations(greenType, greenArray)
+
+	let redArray = []
+	let range2 = new vscode.Range(new vscode.Position(7, 2),
+								  new vscode.Position(7, 6))
+	let redDecoration = {range: range2}
+	redArray.push(redDecoration)
+	editor0.setDecorations(redType, redArray)
+
+	let yellowArray = []
+	let range3 = new vscode.Range(new vscode.Position(0, 6),
+								 new vscode.Position(0, 30))
+	let yellowDecoration = {range: range3}
+	yellowArray.push(yellowDecoration)
+	editor1.setDecorations(yellowType, yellowArray)
+
+}
+
+const greenType = vscode.window.createTextEditorDecorationType({
+	backgroundColor: 'green',
+	// border: '2px solid white',
+	})
+const redType = vscode.window.createTextEditorDecorationType({
+	backgroundColor: 'red',
+	// border: '2px solid white',
+	})
+const yellowType = vscode.window.createTextEditorDecorationType({
+	backgroundColor: '#f29500cc',
+	// border: '2px solid white',
+	})
+
+
 
 // This method is called when your extension is deactivated
 function deactivate() {}
